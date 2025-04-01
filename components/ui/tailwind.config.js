656b4b21e02b2a77b2be433902a73ac2a076c77a
chachi77 <wakuwaku77jp@gmail.com>
2025-04-01 21:13:43 +0900

    Update tailwind.config.js

--- components/ui/tailwind.config.js
+++ components/ui/tailwind.config.js
@@ -11,7 +11,9 @@ module.exports = {
         no: '#ef4444',            // 赤
         selectedYes: '#60a5fa',   // 薄い青
         selectedNo: '#f87171'     // 薄い赤
-      }
+      },
+      backgroundColor: theme => theme('colors'),
+      textColor: theme => theme('colors')
     },
   },
   plugins: [],
