eb3589728cd5368dd664317473fa9d4dc6b98b37
chachi77 <wakuwaku77jp@gmail.com>
2025-04-01 15:17:07 +0900

    Delete app directory

--- app/app/page.tsx
+++
@@ -1,6 +0,0 @@
-"use client";
-import LiarTypeQuiz from "@/components/LiarTypeQuiz";
-
-export default function Home() {
-  return <LiarTypeQuiz />;
-}
--- app/components/ui/card.tsx
+++
@@ -1,9 +0,0 @@
-import * as React from "react";
-
-export function Card({ className, ...props }: React.HTMLAttributes<HTMLDivElement>) {
-  return <div className={`rounded-2xl border p-4 shadow ${className}`} {...props} />;
-}
-
-export function CardContent({ className, ...props }: React.HTMLAttributes<HTMLDivElement>) {
-  return <div className={`p-2 ${className}`} {...props} />;
-}
